71d0ecd7cbead6c484c2820c6dbf5f41db0097dc
Zagir Medzhidov <124031984+SikWeet@users.noreply.github.com>
2025-08-30 16:31:17 +0300

    Add files via upload

--- router/auth.py
+++ router/auth.py
@@ -3,38 +3,39 @@ from fastapi.params import Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
-from cookie import create_access_token, verify_telegram
+from cookie import create_access_token, get_current_user, verify_telegram
 from database.base import get_db
 from database.models import BotMessageTemplate
 from database.query import create_user, get_telegram_user
-from env import BOT_TOKEN
+from env import BOT_TOKEN, BOT_USERNAME
 
 
 router = APIRouter()
-templates = Jinja2Templates(directory="src")
+templates = Jinja2Templates(directory="src/auth")
 
 
 @router.get("/", response_class=HTMLResponse)
 async def auth_index(request: Request, db: AsyncSession = Depends(get_db)):
-	return templates.TemplateResponse("auth.html", {"request": request})
+	get_current_user()
+	return templates.TemplateResponse("auth.html", {"request": request, "bot_username": BOT_USERNAME})
 
-@router.post("/telegram", response_class=HTMLResponse)
+@router.get("/telegram", response_class=HTMLResponse)
 async def auth_telegram(request: Request, db: AsyncSession = Depends(get_db)):
-	form = await request.form()
-	data = dict(form)
-	if not verify_telegram(data.copy(), BOT_TOKEN):
+	params = dict(request.query_params)
+	print("Telegram auth data:", params)
+	if not verify_telegram(params.copy(), BOT_TOKEN):
 		raise HTTPException(status_code=400, detail="Invalid Telegram data")
 	
-	telegram_id = int(data["id"])
+	telegram_id = int(params["id"])
 	user = await get_telegram_user(db, telegram_id)
 	if not user:
 		await create_user(
 			db,
 			telegram_id=telegram_id,
-			first_name=data.get("first_name"),
-			last_name=data.get("last_name"),
-			username=data.get("username")
+			first_name=params.get("first_name"),
+			last_name=params.get("last_name"),
+			username=params.get("username")
 		)
 
 	access_token = create_access_token({"telegram_id": telegram_id})
-	return templates.TemplateResponse("auth.html", {"request": request, "status": "ok", "token": access_token})
+	return templates.TemplateResponse("index.html", {"request": request, "status": "ok", "token": access_token})
